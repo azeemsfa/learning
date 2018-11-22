d86e3bf9d198f35e1be8d517b23729d6b543b4f9
azeemsfa <sam.hima@gmail.com>
2018-11-22 14:15:03 -0600

    this update to sent email

--- chkstatus.sh
+++ chkstatus.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-ping 80.80.80.80
+ping -c 2 $1
 
- if [$? -eq 0]
- then
-	echo "server is up and running"
+ if [ $? -eq 0 ]; then
+	echo "server is up and running" >> mailbody.txt
+	ssmtp sam.hima@gmail.com < mailbody.txt
+	rm -rf mailbody.txt
  else
-	echo "some thing went wrong"
+	echo "some thing went wrong" >> mailbody.txt
+        ssmtp sam.hima@gmail.com < mailbody.txt
+        rm -rf mailbody.txt
+
  fi
 
