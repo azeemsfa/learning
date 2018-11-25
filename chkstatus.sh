3272a4e1c64da70a38be51e7331e64857ccc197a
azeemsfa <sam.hima@gmail.com>
2018-11-25 02:55:34 -0600

    update ti check the status of tomcat

--- chkstatus.sh
+++ chkstatus.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-ping -c 2 $1
+#ping -c 2 $1
+wget http://localhost:8080
 
  if [ $? -eq 0 ]; then
 	echo "server is up and running" >> mailbody.txt
 	ssmtp sam.hima@gmail.com < mailbody.txt
+	rm -rf index.html
 	rm -rf mailbody.txt
  else
 	echo "some thing went wrong" >> mailbody.txt
         ssmtp sam.hima@gmail.com < mailbody.txt
-        rm -rf mailbody.txt
-
+        rm -rf index.html
+	rm -rf mailbody.txt
  fi
 
